b0e0be27b16b17dc31d37d32105079e5bbc53f0b
domager73 <112970685+domager73@users.noreply.github.com>
2023-01-29 18:42:55 +0300

    Dima1

--- ConsoleAppSpamFilter/Program.cs
+++ ConsoleAppSpamFilter/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string[] LoadStopWords(string fileName) 
+{
+    string[] stopWords;
 
-Console.WriteLine("Hello, World!");
+    StreamReader reader = new StreamReader(fileName);
+
+    int lenght = int.Parse(reader.ReadLine());
+
+    stopWords = new string[lenght];
+
+    for (int i = 0; i < stopWords.Length; i++) 
+    {
+        stopWords[i] = reader.ReadLine();
+    }
+
+    reader.Close();
+
+    return stopWords;
+}
+
+string LoadText(string fileName)
+{
+    string text;
+
+    StreamReader reader = new StreamReader(fileName);
+
+    text = reader.ReadToEnd();
+
+    reader.Close();
+
+    return text;
+}
+
+string[] stopWords = null;
+string text;
+
+stopWords = LoadStopWords("stopList.txt");
+text = LoadText("text.txt");
